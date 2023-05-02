a5294fc1f068523e80e31067c65ea910438ce1a2
Laurion <klauriondareich@gmail.com>
2023-05-02 14:25:00 +0100

    update made to resolve the issue noticed when we share training links from fb

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -1,13 +1,7 @@
 const data_btn = document.querySelectorAll('[data-training-btn]');
 
-let param = window.location.href.split("?")[1];
+let param = window.location.href.split("?")[1].split("-kopa")[0];
 
 if (param) localStorage.setItem('training_key', param)
 
-// for(let element of data_btn) {
-
-//     element.addEventListener('click', () => {
-//         localStorage.setItem('training_key', element.dataset.trainingBtn)
-//     })
-// }
 
